5d1d02455bb492151e62640530cd831007c9ec51
ritam4735 <183220555+ritam4735@users.noreply.github.com>
2026-06-17 22:12:49 +0530

    week3 day1 solutions

---
+++ Week_3/Day_1/q2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next; // save next node
+            curr.next = prev;          // reverse link
+
+            prev = curr;               // move prev
+            curr = next;               // move curr
+        }
+
+        return prev;
+    }
+}
